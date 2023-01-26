7f2a2e82db4e06a7a7bc9c08a2aa78a03d9ab1da
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2023-01-26 09:26:55 +0300

    Добавьте файлы проекта.

---
+++ Testing/Components/Navigation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Testing.Components
+{
+    internal class Navigation
+    {
+        public static List<Nav> navs = new List<Nav>();
+        public static User AutoUser;
+        public static MainWindow main;
+        public static void Update(Nav nav) 
+        {
+            main.Myframe.Navigate(nav.Page);
+           
+        }
+        public static void Nextpage(Nav nav) 
+        {
+            navs.Add(nav);
+            Update(nav);
+        }
+    }
+    class  Nav
+    {
+     //public string Title { get; set;}
+        public Page Page { get; set; }
+        public Nav( Page Page) 
+        {
+          /*  this.Title = Title*/
+            this.Page = Page;   
+        }
+    }
+}
+
---
+++ Testing/Pages/AuthPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Testing.Components;
+using Testing.Pages;
+
+namespace Testing.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text.Trim();
+            string password = PassworTb.Password.Trim();
+            if (login.Length > 0 && password.Length > 0)
+            {
+                Navigation.AutoUser = DBConnect.db.User.ToList().Find(x => x.Password == password && x.Login == login);
+                if (Navigation.AutoUser == null)
+                {
+                    MessageBox.Show("Вы кто");
+                }
+                else {
+                    Navigation.Nextpage(new Nav(new CalendarPage()));
+                }
+            }
+            else MessageBox.Show("Заполните поля");
+        }
+    }
+}
